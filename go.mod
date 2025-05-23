a0d535ae6e4979eb73dc2a8f0366110cbc557223
KurepinVladimir <qrepin@gmail.com>
2025-05-23 12:08:45 +0300

    Первый коммит

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/voffka/parser
+
+go 1.24.3
+
+require github.com/PuerkitoBio/goquery v1.10.3
+
+require github.com/suyashkumar/dicom v1.0.3
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/chromedp/cdproto v0.0.0-20250518235601-40b4c35ec9fe // indirect
+	github.com/chromedp/chromedp v0.13.6
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250517221953-25912455fbc8 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
---
+++ mail.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	// Создаём контекст Chrome
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Добавим таймаут на всякий случай
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var html string
+
+	// URL страницы
+	url := "https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/doors---20/extras---130/extras---20/extras---50/gearbox---1/mileage_max---90000/year_min---66/?price_min=6000&price_max=12500"
+
+	// Запускаем браузер и получаем HTML после полной загрузки страницы
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Sleep(5*time.Second), // ждём загрузку JS
+		chromedp.OuterHTML("body", &html),
+	)
+	if err != nil {
+		log.Fatalf("Ошибка при загрузке страницы: %v", err)
+	}
+
+	// Загружаем в goquery
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(html)))
+	if err != nil {
+		log.Fatalf("Ошибка парсинга HTML: %v", err)
+	}
+
+	fmt.Println("Объявления:")
+	// Для каждого блока объявления
+	doc.Find(".advert__content").Each(func(i int, s *goquery.Selection) {
+		// Название
+		titleSel := s.Find("a.advert__content-title")
+		title := strings.TrimSpace(titleSel.Text())
+		link, _ := titleSel.Attr("href")
+
+		// Цена
+		price := strings.TrimSpace(s.Find("a.advert__content-price").Text())
+
+		//<span><b>€</b>12.500  <span class="advert__content-price--discount"><b>€</b>13.200</span></span>
+		priceNode := s.Find("a.advert__content-price > span").First()
+		priceNode.Find(".advert__content-price--discount").Remove()
+		price2 := strings.TrimSpace(priceNode.Text())
+
+		// Выводим, если всё есть
+		if title != "" && link != "" && price != "" {
+			fmt.Printf("%d. %s\n", i+1, title)
+			fmt.Println("   Ссылка: https://www.bazaraki.com" + link)
+			fmt.Println("   Цена:  ", price)
+			fmt.Println("   Цена (итоговая):  ", price2)
+		}
+	})
+
+}
